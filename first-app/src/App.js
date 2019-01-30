03680e4d8f4e2edb17c0ad2210e7e5800d561c38
pramod.kharade <pramod.kharade@blazeclan.com>
2019-01-30 17:43:34 +0530

    Props & State in custom componenet

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -4,13 +4,21 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    Person : [
+      {name:'Pramod',age:30},
+      {name:'Shital',age:29},
+      {name:'Kishor',age:30}
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h2>Hi, I am react App!</h2>
-        <Person name="Pramod" age="30"></Person>
-        <Person name="Shital" age="29">My Hobbies: Playing with New Technologies!</Person>
-        <Person name="Kishor" age="30"></Person>
+        <button>Switch Name</button>
+        <Person name={ this.state.Person[0].name} age={ this.state.Person[0].age}></Person>
+        <Person name={ this.state.Person[1].name} age={ this.state.Person[1].age}> My Hobbies: Playing with New Technologies</Person>
+        <Person name={ this.state.Person[2].name} age={ this.state.Person[2].age}></Person>
       </div>
     );
   }
