8b387dbf263132feba28e6eaf0409cc9e7e19d8b
pramodkharade <kharade.pramod91@gmail.com>
2020-05-07 20:36:24 +0530

    namehandler on change of person input

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -41,24 +41,21 @@ class App extends Component {
     });
     console.log('Delete handler Calling');
   };
-  nameChangedHandler = (event) => {
+  nameChangedHandler = (event, id) => {
     //console.log("was click Event");
     // Don't Do this: this.state.Persons[0].name = "Pramod Kharade";
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+    const person = {
+      ...this.state.persons[personIndex]
+    }; // approach 2
+    // const person = Object.assign({},this.state.persons[personIndex]); // approach 2
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
     this.setState({
-      persons: [
-        {
-          name: "Dyanada Pramod Kharade",
-          age: 30
-        },
-        {
-          name: event.target.value,
-          age: 29
-        },
-        {
-          name: 'Kishor',
-          age: 30
-        }
-      ]
+      persons: persons
     });
   }
 
@@ -75,7 +72,15 @@ class App extends Component {
       person = (
         <div>
           {this.state.persons.map((person, index) => {
-          return <Person name={person.name} age={person.age} key={person.id} click={() => this.deletePersonHandler(index)}/>
+          return <Person
+            name={person.name}
+            changed={(event) => {
+              this.nameChangedHandler(event, person.id)
+            }}
+            age={person.age}
+            key={person.id}
+            click={() => this.deletePersonHandler(index)}
+            />
         })}
         
           </div>
