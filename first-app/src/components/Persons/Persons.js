4382c4ee52a27c0652c547f91f7b211ba64c3617
pramodkharade <kharade.pramod91@gmail.com>
2020-05-09 13:23:29 +0530

    class based life cycle hooks to maintain state

--- first-app/src/components/Persons/Persons.js
+++ first-app/src/components/Persons/Persons.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
 import Person from './Person/Person';
 class Persons extends Component {
+  // static getDerivedStateFromProps(props, state) {
+  //   return state;
+  // }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[Persons.js] shouldComponetUpdate');
+    return true;
+  }
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('[Persons.js] getSnapshotBeforeUpdate');
+    return {
+      message: 'snapshot!'
+    };
+  }
+  componentDidMount() {
+    console.log('[Persons.js] componetDidMount');
+  }
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('[Persons.js] componentDidUpdate');
+    console.log(snapshot);
+
+  }
   render() {
     return this.props.persons.map((person, index) => {
       return <Person
