b9893f8d3756e77d6e8d3e8ffeeade8072a6f271
pramodkharade <kharade.pramod91@gmail.com>
2020-05-11 09:59:30 +0530

    Higher Order Components for wrapping multiple elements

--- first-app/src/components/Persons/Person/Person.js
+++ first-app/src/components/Persons/Person/Person.js
@@ -1,15 +1,17 @@
 import React, { Component, PureComponent } from 'react';
 
 import './Person.css';
+import Auxiliary from '../../../hoc/Auxiliary';
 class Person extends PureComponent {
   render() {
     return (
-      <div className="Person" >
+      //<div className="Person" >
+      <Auxiliary>
           <p
       onClick={this.props.click}> I'm a {this.props.name} and  I am  {this.props.age} years old!</p>
           <p>{this.props.children}</p>
           <input type="text" onChange={ this.props.changed} value={this.props.name}/>
-      </div>
+     </Auxiliary> // </div>
       );
   }
 }
