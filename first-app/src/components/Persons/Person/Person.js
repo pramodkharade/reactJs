f6327eb838de6cad1a9b0c915ce69af7d09a89c8
pramodkharade <kharade.pramod91@gmail.com>
2020-05-11 11:23:52 +0530

    refs for calling function on events

--- first-app/src/components/Persons/Person/Person.js
+++ first-app/src/components/Persons/Person/Person.js
@@ -3,6 +3,15 @@ import PropTypes from 'prop-types';
 import './Person.css';
 import Auxiliary from '../../../hoc/Auxiliary';
 class Person extends PureComponent {
+
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+  componentDidMount() {
+    // this.inputElement.focus();
+    this.inputElementRef.current.focus();
+  }
   render() {
     return (
       //<div className="Person" >
@@ -10,7 +19,10 @@ class Person extends PureComponent {
           <p
       onClick={this.props.click}> I'm a {this.props.name} and  I am  {this.props.age} years old!</p>
           <p>{this.props.children}</p>
-          <input type="text" onChange={ this.props.changed} value={this.props.name}/>
+          <input type="text" onChange={ this.props.changed}
+      // ref ={(inputEl)=> this.inputElement = inputEl}
+      ref= {this.inputElementRef}
+      value={this.props.name}/>
      </Auxiliary> // </div>
       );
   }
